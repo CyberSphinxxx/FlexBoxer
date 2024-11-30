79a44635ca5973485aba2ddfcf430e99b17d15f1
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-11-30 10:15:03 +0800

    Feature: Random Color on Add

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ function removeBox() {
     }
 }
 
+function getRandomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function addBox() {
+    boxCount++;
+    const newBox = document.createElement('div');
+    newBox.className = `box box${boxCount}`;
+    newBox.textContent = `Box ${boxCount}`;
+    newBox.style.backgroundColor = getRandomColor();
+    boxContainer.appendChild(newBox);
+}
